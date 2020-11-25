1db4024edc394019d0cfd12c82af37191bff9303
RUBENLEONARDO SIGALINGGING <74916771+bensleonardsmart@users.noreply.github.com>
2020-11-25 10:17:30 -0800

    OPERATOR ARITMATIKA

---
+++ OPERATOR ARITMATIKA.py
@@ -0,0 +1,28 @@
+# AMBIL INPUT UNTUK MENGISI NILAI NYA 
+c = 0
+a = int(input("==> OKE OM INPUTKAN NILAI A : "))
+b = int(input("==> OKE OM INPUTKAN NILAI B : "))
+
+# MASUK KE OPERASI PENJUMLAHAN
+c = a + b
+print ("HASIL DARI A + B = " + str(c))
+
+# MASUK KE OPERASI PENGURANGAN
+c = a - b
+print ("HASIL DARI A - B = " + str(c))
+
+# MASUK KE OPERASI PERKALIAN
+c = a * b
+print ("HASIL DARI A * B = " + str(c))
+
+# MASUK KE OPERASI PEMBAGIAN
+c = a / b
+print ("HASIL DARI A / B = " + str(c))
+
+# MASUK KE OPERASI PANGKAT
+c = a ** b
+print ("a ** b = " + str(c))
+
+# MASUK KE OPERASI SISA BAGI (MODULUS)
+c = a % b
+print ("HASIL DARI A % B = " + str(c))
