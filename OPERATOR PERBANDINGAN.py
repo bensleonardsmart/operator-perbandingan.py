9b2ec5f6761756107bedd5f844f38b81470cdeab
RUBENLEONARDO SIGALINGGING <74916771+bensleonardsmart@users.noreply.github.com>
2020-11-25 10:19:44 -0800

    OPERATOR PERBANDINGAN
    ==> MY AKUN GITHUB :
    https://github.com/bensleonardsmart
    
    ==> MY AKUN BLOGGER :
    https://otakteknologiinformasi.blogspot.com/
    
    ==> MY AKUN INSTAGRAM :
    https://www.instagram.com/invites/contact/?i=eibdkzz5u98j&utm_content=dx5lfe3
    
    ==> MY AKUN YOUTUBE :
    RUBEN LEONARDO SIGALINGGING*
    https://youtu.be/8JLHGdZEnrk

--- OPERATOR PERBANDINGAN.py
+++ OPERATOR PERBANDINGAN.py
@@ -1,23 +1,43 @@
-a = int(input("===> MASUKAN NILAI A NYA OM <=== : "))
-b = int(input("===> MASUKAN NILAI B NYA OM <=== : "))
+import time
 
-print ("===> CEK PERSAMAAN NYA NYA")
+a = int(input("===> MASUKAN NILAI A NYA OM <=== : "))
+time.sleep(5)
+b = int(input("===> MASUKAN NILAI B NYA OM <=== : "))
+time.sleep(5)
+
+print ("|||||||||||||||||||||||||||||")
+time.sleep(2)
+print ("===> CEK PERSAMAAN NYA NYA : ")
+time.sleep(5)
 if(a==b):
 	print ("NILAI A SAMA DENGAN NILAI B")
+	time.sleep(5)
 else:
 	print ("NILAI A TIDAK SAMA DENGAN NILAI B")
-print ("CEK PERTIDAKSAMAAN NYA")
+	time.sleep(5)
+print ("CEK PERTIDAKSAMAAN NYA : ")
+time.sleep(5)
 if(a!=b):
 	print ("===> NILAI A TIDAK SAMA DENGAN NILAI B")
+	time.sleep(5)
 else:
 	print ("===> NILAI A SAMA DENGAN NILAI B")
-print ("CEK KURANG DARI")
+	time.sleep(5)
+print ("CEK KURANG DARI : ")
+time.sleep(5)
 if(a<b):
-	print ("===> NILAI A KURANG DARI B")
+	print ("===> NILAI A KURANG DARI NILAI B")
+	time.sleep(5)
 else:
-	print ("===> NILAI A TIDAK KURANG DARI B")
-print ("CEK LEBIH DARI ATAU SAMA DENGAN")
+	print ("===> NILAI A TIDAK KURANG DARI NILAI B")
+	time.sleep(5)
+print ("CEK LEBIH DARI ATAU SAMA DENGAN : ")
+time.sleep(5)
 if(a>b):
-	print ("NILAI A LEBIH BESAR DARI B")
+	print ("===> NILAI A LEBIH BESAR DARI B")
+	time.sleep(5)
 else:
-	print ("===> NILAI A KURANG  DARI B")
+	print ("===> NILAI A KURANG  DARI NILAI B")
+	time.sleep(5)
+print ("SELESAI...")
+time.sleep(5)
